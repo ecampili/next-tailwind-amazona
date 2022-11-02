e591a9caaf22cc59850cb90b17e8fc331a4d61fc
workcampili <work.campili@gmail.com>
2022-11-02 14:43:41 -0300

    create Layout

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 2. Install Tools
 3. Create Next App
 4. Publish to Github
+5. Create Website Layout
+   a) create layout component
+   b) add header
+   c) add main section
+   d) add footer
+   e) add tailwind classes
---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Layout({ children, title }) {
+    return (
+        <>
+            <Head>
+                <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+                <meta name="description" content="Ecommerce Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className='flex min-h-screen flex-col justify-between'>
+                <header>
+                    <nav className='flex h-12 justify-between shadow-md items-center px-4'>
+                        <Link href='/' className='text-lg font-bold'>
+                            amazona
+                        </Link>
+                        <div >
+                            <Link href='/cart' className='p-2'>
+                                Cart
+                            </Link>
+                            <Link href='/login' className='p-2'>
+                                Login
+                            </Link>
+                        </div>
+                    </nav>
+                </header>
+                <main className='container m-auto mt-4 px-4'>
+                    {children}
+                </main>
+                <footer className='flex  shadow-inner justify-center items-center h-10 bg-black text-white'>
+                    <p>Copyright &copy; 2022 Amazona</p>
+                </footer>
+            </div>
+        </>
+    )
+}
